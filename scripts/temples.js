6105e2adc62183a347988ea1af493619d455f6a4
Kingsley Ani <your-email@example.com>
2026-01-17 18:29:56 +0100

    Album

---
+++ scripts/temples.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector(".hamburger")
+const menu = document.createElement("div");
+menu.classList.add("menu");
+const menuItems = ["Home", "Old", "New", "Large", "Small"];
+const ul = document.createElement("ul");
+
+menuItems.forEach(item => {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.href = "#";           // replace with real links
+  a.textContent = item;
+  li.appendChild(a);
+  ul.appendChild(li);
+});
+menu.appendChild(ul);
+const body = document.body
+body.appendChild(menu);
+
+hamburger.addEventListener("click", ()=>{
+hamburger.classList.toggle("active")
+
+ menu.classList.toggle("active");
+});
+

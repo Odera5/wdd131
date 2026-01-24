0d0b069d55c357c86e2704d7ce36ef530fb47860
Kingsley Ani <your-email@example.com>
2026-01-24 22:43:02 +0100

    week3

---
+++ scripts/place.js
@@ -0,0 +1,21 @@
+const temperature = 28; // Celsius
+const windSpeed = 10;   // km/h
+
+function calculateWindChill(temp, speed) {
+  return (
+    13.12 +
+    0.6215 * temp -
+    11.37 * Math.pow(speed, 0.16) +
+    0.3965 * temp * Math.pow(speed, 0.16)
+  ).toFixed(1);
+}
+
+let windChill = "N/A";
+
+if (temperature <= 10 && windSpeed > 4.8) {
+  windChill = calculateWindChill(temperature, windSpeed) + " °C";
+}
+
+document.getElementById("windchill").textContent = windChill;
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;

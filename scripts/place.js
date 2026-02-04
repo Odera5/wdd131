01ef203c8236263cdc77635c8be0896fdc00b27a
Kingsley Ani <your-email@example.com>
2026-02-04 13:29:44 +0100

    wk 5

--- scripts/place.js
+++ scripts/place.js
@@ -1,21 +1,31 @@
-const temperature = 28; // Celsius
-const windSpeed = 10;   // km/h
+document.addEventListener("DOMContentLoaded", ()=>{
 
-function calculateWindChill(temp, speed) {
-  return (
-    13.12 +
-    0.6215 * temp -
-    11.37 * Math.pow(speed, 0.16) +
-    0.3965 * temp * Math.pow(speed, 0.16)
-  ).toFixed(1);
+const data = document.querySelector(".data");
+const temp = document.querySelector(".temp");
+let arr1 = ["Area","Population", "Capital", "Language", "Currency", "Time Zone", "Calling Code"];
+let arr2 = ["357,600","84,000000","Berlin", "German","Euro","GMT + 1","+42"];
+let arrTemp1 = ["Temperature", "Conditions", "Wind", "Wind Chill"];
+let arrTemp2 = ["10c", "Partly Cloudy", "5k/h", "9.8c"]
+
+const ul1 = document.createElement("ul");
+data.appendChild(ul1);
+const ul2 = document.createElement("ul");
+temp.appendChild(ul2);
+
+for(let i=0; i<arr1.length; i++){
+  const li = document.createElement("li");
+  li.innerHTML = `<strong>${arr1[i]}</strong>: ${arr2[i]}`;
+  ul1.appendChild(li);
 }
 
-let windChill = "N/A";
-
-if (temperature <= 10 && windSpeed > 4.8) {
-  windChill = calculateWindChill(temperature, windSpeed) + " °C";
+for(let i=0; i<arrTemp1.length; i++){
+  const li = document.createElement("li");
+  li.innerHTML = `<strong>${arrTemp1[i]}</strong>: ${arrTemp2[i]}`;
+  ul2.appendChild(li);
 }
 
-document.getElementById("windchill").textContent = windChill;
-document.getElementById("year").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
+const  span = document.querySelector(".copyright");
+span.textContent = new Date().getFullYear();
+const lastModified = document.querySelector(".lastModified");
+lastModified.textContent = 'Last Modified:' + " " + document.lastModified;
+})
